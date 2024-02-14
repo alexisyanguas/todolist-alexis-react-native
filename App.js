70a831745371327e22146e15a76d728f8ef8b6c9
Alexis Yanguas <yanguasalexis@gmail.com>
2024-02-14 15:32:29 +0100

    Step 3

--- App.js
+++ App.js
@@ -1,25 +1,58 @@
 import React, { useState } from "react";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TextInput, View, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Button,
+  Pressable,
+} from "react-native";
 
 export default function App() {
+  const [inputValue, setInputValue] = useState(""); // [state, setState
   const [objectives, setObjectives] = useState([
     { name: "Buy Milk" },
     { name: "Go to the gym" },
     { name: "Learn React Native" },
   ]);
 
+  const addObjective = () => {
+    if (!inputValue) return;
+
+    setObjectives([{ name: inputValue }, ...objectives]);
+    setInputValue("");
+  };
+
+  const removeObjective = (index) => {
+    const newObjectives = objectives.filter((_, i) => i !== index);
+    setObjectives(newObjectives);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.form}>
-        <TextInput style={styles.input} placeholder="Add objective" />
-        <Button title="Add" />
+        <TextInput
+          style={styles.input}
+          placeholder="Add objective"
+          value={inputValue}
+          onChangeText={setInputValue}
+        />
+        <Button title="Add" onPress={() => addObjective()} />
       </View>
       <View style={styles.objectives}>
         {objectives.map((objective, key) => (
           <View style={styles.objective} key={key}>
             <Text style={styles.text}>{objective.name}</Text>
+            <Pressable
+              onPress={() => removeObjective(key)}
+              style={styles.removeIcon}
+            >
+              {/* X-Twitter */}
+              <FontAwesome5 name="twitter" size={24} color="red" />
+            </Pressable>
           </View>
         ))}
       </View>
@@ -37,7 +70,6 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   text: {
-    color: "red",
     fontWeight: "bold",
   },
   input: {
@@ -59,5 +91,10 @@ const styles = StyleSheet.create({
     margin: 10,
     backgroundColor: "#f0f0f0",
     borderRadius: 5,
+  },
+  removeIcon: {
+    position: "absolute",
+    right: 5,
+    top: 5,
   },
 });
